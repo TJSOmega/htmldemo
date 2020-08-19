714b3cfdda01c68653203909b558c4a259c9c168
Terrence Jamal Simms <Tjs@omegacc.com>
2020-08-18 18:33:50 -0700

    Most recent edits to app.js file

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-var dogtype = prompt('What dogs do you like the most? Big Dogs, Medium Dogs, Small Dogs');
-var dogresponse;
+var dogtype = prompt('What dogs do you like the most? Big Dogs, Medium Dogs, or Small Dogs');
+    var dogresponse;
+
+function dogsurvey() {
 
 if (dogtype == 'Big Dogs') {
     dogresponse = 'Big Dogs are great';
-} else if (dogtype == 'Medium Dogs') {
-    dogresponse = 'I Love Medium Dogs'
-} else if (dogtype == 'Small Dogs') {
-        dogresponse = ('Ohh you picked the wrong type')
-    }
-    else {
-    dogresponse = ("Invalid Response")
+    } else if (dogtype == 'Medium Dogs') {
+    dogresponse = 'I Love Medium Dogs';
+    } else if (dogtype == 'Small Dogs') {
+        dogresponse = 'Ohh you picked the wrong type';
+    } else {
+    dogresponse = 'Invalid Response';
     }
 
     console.log ("evaluation ", dogresponse)
     document.write ('your response ', dogresponse)
+}
+
+dogsurvey
+
+dogsurvey
+
+dogsurvey
+
+
+    
