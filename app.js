ed5141d1bbe2bd36410ccdf09f7f30f27df831b7
Terrence Jamal Simms <Tjs@omegacc.com>
2020-09-30 19:54:43 -0700

    Added files

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-var dogtype = prompt('What dogs do you like the most? Big Dogs, Medium Dogs, or Small Dogs');
-    var dogresponse;
-
-function dogsurvey() {
-
-if (dogtype == 'Big Dogs') {
-    dogresponse = 'Big Dogs are great';
-    } else if (dogtype == 'Medium Dogs') {
-    dogresponse = 'I Love Medium Dogs';
-    } else if (dogtype == 'Small Dogs') {
-        dogresponse = 'Ohh you picked the wrong type';
-    } else {
-    dogresponse = 'Invalid Response';
+function Dognumber() {
+    var dogamount = prompt('How many dogs do you own?')
+    var response
+    var invalid = prompt('Sorry your response was invald')
+    
+    if (dogamount === 1) {
+    response = ('Ohh 1 dog is great!');
+    } else if (dogamount === 2) {
+    response = ('Ohh boy 2 dogs is even better!')
+    } else if (dogamount === 3) {
+    response = ('Wow 3 dogs! That is the best!')
+    }else {
+    response = ('Invalid selection')
     }
 
-    console.log ("evaluation ", dogresponse)
-    document.write ('your response ', dogresponse)
+    console.log('Dog amount listed as')
+    document.write('So cool to see you own ' + dogamount)
 }
 
-dogsurvey
+function changecolor() {
+    var backcolor = prompt('What is your fav color??')
+    var el = document.body.style.background = backcolor
+}
 
-dogsurvey
-
-dogsurvey
+//
 
 
-    
+
+Dognumber()
+changecolor()
